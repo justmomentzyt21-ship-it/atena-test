05f9dbf3a1688a8c2f14f8e9f4546df2de768158
Tomás Romero <tomasrom.dev@gmail.com>
2026-08-11 23:41:21 -0300

    up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,11 @@ import { routes } from './app.routes';
 import { PRODUCT_REPOSITORY } from './core/tokens/product-repository.token';
 import { GoogleSheetsProductAdapter } from './core/services/google-sheets-product.adapter';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
 import { BANNER_REPOSITORY } from './core/tokens/banner-repository.token';
 import { GoogleSheetsBannerAdapter } from './core/services/google-sheets-banner.adapter';
 import { provideServiceWorker } from '@angular/service-worker';
@@ -16,7 +20,12 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()),
     { provide: PRODUCT_REPOSITORY, useClass: GoogleSheetsProductAdapter },
     { provide: BANNER_REPOSITORY, useClass: GoogleSheetsBannerAdapter },
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(
+      withEventReplay(),
+      withHttpTransferCacheOptions({
+        includePostRequests: false,
+      }),
+    ),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
